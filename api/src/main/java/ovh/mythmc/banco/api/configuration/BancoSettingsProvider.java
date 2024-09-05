efbb1a7e426d1b5febbc3a9126ea6ed46d6534f5
U8092 <uisl8092@gmail.com>
2024-09-05 17:57:01 +0200

    fix: cached banco items should be cleared when reloading

--- api/src/main/java/ovh/mythmc/banco/api/configuration/BancoSettingsProvider.java
+++ api/src/main/java/ovh/mythmc/banco/api/configuration/BancoSettingsProvider.java
@@ -27,7 +27,7 @@ public final class BancoSettingsProvider {
                 YamlConfigurationProperties.newBuilder().charset(StandardCharsets.UTF_8).build()
         );
 
-        get().getCurrency().getItems().clear();
+        Banco.get().getItemManager().clear();
         get().getCurrency().getItems().forEach(bancoItem -> Banco.get().getItemManager().registerItems(bancoItem));
     }
 
